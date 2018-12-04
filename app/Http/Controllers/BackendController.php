4ec4317d436e93eb60cfc0cadc6e263a2f3af0d7
Hicham <hicham@iMac-van-Hicham-2.local>
2018-12-04 01:34:34 +0100

    admin view aangemaakt

---
+++ app/Http/Controllers/BackendController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BackendController extends Controller
+{
+    //
+
+     public function dashboard(){
+
+
+         return view('admin.dashboard');
+
+
+     }
+
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,12 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/admin', [
+
+    'uses' => 'BackendController@dashboard',
+    'as' => 'admin.dashboard'
+
+
+
+]);
